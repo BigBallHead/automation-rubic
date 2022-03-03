86d15998029f6260e1e9451dbe686c718d58d38e
BigBallHead <ivanukondratev@gmail.com>
2022-03-03 10:53:08 +0300

    fix doc

--- functions.py
+++ functions.py
@@ -1,8 +1,8 @@
-from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome(executable_path='/Users/ivankondratev/Desktop/automation.rubic/metamaskextgeneral.crx')
 
