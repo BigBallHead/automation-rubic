cbf7b398ebde2e88c8484bc00fca97c232d7cc20
BigBallHead <ivanukondratev@gmail.com>
2022-08-24 11:12:47 +0300

    update test-case

--- test_case_3_find_token.py
+++ test_case_3_find_token.py
@@ -2,16 +2,24 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from main import wait_elements_clicable
 from selenium.common.exceptions import NoSuchElementException
+from time import sleep
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+def check_token():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
 
-driver.get("https://app.rubic.exchange/")
-wait_elements_clicable(xpath="//*[@id='token1']", driver=driver).click()
-driver.find_element_by_xpath("//input[@tabindex='0']").send_keys("USDC")
-try:
-    usdc_coin = driver.find_element_by_xpath("//*[@alt='USD Coinыыыы']")
-    print("I can find this coin!!!")
-except NoSuchElementException:
-    print("I cannot find this coin ((")
+    driver.get("https://app.rubic.exchange/")
+    wait_elements_clicable(xpath="//*[@id='token1']", driver=driver).click()
+    driver.find_element_by_xpath("//input[@tabindex='0']").send_keys("USDC")
+    try:
+        usdc_coin = driver.find_element_by_xpath("//*[@alt='USD Coin']")
+        print("I can find this coin!!!")
+    except NoSuchElementException:
+        print("I cannot find this coin ((")
 
-driver.quit()
+    driver.quit()
+
+if __name__ == '__main__':
+
+    while True:
+        check_token()
+        sleep(3600)
