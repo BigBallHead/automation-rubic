1b8c8927887cd4902d47badd4b9b8c3c3b63131c
BigBallHead <ivanukondratev@gmail.com>
2022-08-26 16:31:18 +0300

    rename "driver"

--- test_case_3_find_token.py
+++ test_case_3_find_token.py
@@ -5,19 +5,21 @@ from selenium.common.exceptions import NoSuchElementException
 from time import sleep
 
 def check_token():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    browser = webdriver.Chrome(ChromeDriverManager().install())
 
-    driver.get("https://app.rubic.exchange/")
-    wait_elements_clicable(xpath="//*[@id='token1']", driver=driver).click()
-    driver.find_element(by='xpath', value=("//input[@tabindex='0']")).send_keys("USDC")
+    browser.get("https://app.rubic.exchange/")
+    wait_elements_clicable(xpath="//*[@id='token1']", driver=browser).click()
+    browser.find_element(by='xpath', value=("//input[@tabindex='0']")).send_keys("USDC")
     sleep(2)
     try:
-        usdc_coin = driver.find_element(by='xpath', value=("//*[@alt='USD Coin']"))
+        usdc_coin = browser.find_element(by='xpath', value=("//*[@alt='USD Coin']"))
         print("Token search is alive!!!")
     except NoSuchElementException:
         print("I cannot find this coin ((")
 
-    driver.quit()
+    browser.quit()
+
+check_token()
 
 if __name__ == '__main__':
 
